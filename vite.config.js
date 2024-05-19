7ee243a9ce20430e356fd258afbd8492bc8c026f
manulucena <mlucenasalas@gmail.com>
2024-05-19 15:33:37 +0200

    d7

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://6649fe06d8a3d0b8366cbf17--admirable-crisp-162228.netlify.app/',
+  base: 'https://creative-khapse-8f159c.netlify.app/',
   plugins: [react()],
 });
 
